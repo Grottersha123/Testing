3728bb0e8a200c490b98c72f5e28a2690ff0d92e
Grottersha <grottersha123@gmail.com>
2017-04-05 17:02:06 +0300

    Добавлены отычеты

---
+++ lab1/UnitTestLab1/UnitTestLab1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab1;
+
+namespace UnitTestLab1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Module1.doSort(""), "");
+            Assert.AreEqual(Module1.doSort("32 1 4"), "1 4 32 ");
+            Assert.AreEqual(Module1.doSort("32sd 1 4sd sda"), "1 4 32 ");
+            Assert.AreEqual(Module1.doSort("32 1 asd"), "1 32 ");
+            
+        }
+    }
+}
